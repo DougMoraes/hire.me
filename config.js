b54a4f00124437710234265c6017b9b8871ab3cd
Douglas Moraes <douglas@oc5182036478.ibm.com>
2018-05-12 03:11:50 -0300

    initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.db = {};
+config.webhost = 'http://localhost:3000/';
+
+config.db.host = 'mongodb://bemobi:123456@ds031895.mlab.com:31895/url-shortener-doug';
+
+config.db.name = 'url_shortener';
+
+module.exports = config;
