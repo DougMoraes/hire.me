2cb5f76b5031724b9f7801b2cd7d373cdd8892a6
Douglas Moraes <douglas@oc5182036478.ibm.com>
2018-05-12 22:38:48 -0300

    feat: add logic to check if alias is already taken

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ var Url = require('./models/url');
 mongoose.connect(config.db.host);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
@@ -26,36 +23,54 @@ app.post('/api/shorten', function (req, res) {
 
   Url.findOne({ long_url: longUrl }, function (err, doc) {
     if (doc) {
-      console.log("Doug", doc);
+      res.send({ 'shortUrl': doc.short_url });
 
-      res.send({
-        'shortUrl': doc.short_url
-      });
     } else {
-
       var newUrl = Url({
         long_url: longUrl,
         short_url: shortUrl
       });
 
-      console.log("Doug", shortUrl);
-
       if (shortUrl == "" || shortUrl == undefined) {
-        newUrl.short_url = base58.encode(Math.random() * 100)
-      } else {
-        newUrl.short_url = shortUrl
-      }
-
-      newUrl.save(function (err) {
+        newUrl.short_url = base58.encode(Math.random() * 100);
         
-        if (err) {
-          console.log(err);
-        }
+        newUrl.save(function (err) {
+          if (err) {
+            console.log(err);
 
-        res.send({
-          'shortUrl': newUrl.short_url
+          }
+  
+          res.send({
+            'shortUrl': newUrl.short_url
+          
+          });
         });
-      });
+      } else {
+        Url.findOne({ short_url: shortUrl }, function (err, doc) {
+          if (doc) {
+            var erro = {
+              alias: shortUrl,
+              err_code: "001",
+              description: "CUSTOM ALIAS ALREADY EXISTS"
+            };
+
+            res.send(erro);
+          } else {
+            newUrl.short_url = shortUrl
+            newUrl.save(function (err) {
+        
+              if (err) {
+                console.log(err);
+              }
+      
+              res.send({
+                'shortUrl': newUrl.short_url
+              });
+            });
+          }
+        })
+        
+      }
     }
   });
 });
--- public/javascripts/shorten.js
+++ public/javascripts/shorten.js
@@ -6,9 +6,15 @@ $('.btn-shorten').on('click', function(){
     dataType: 'JSON',
     data: {url: $('#url-field').val(), alias: $('#alias-field').val()},
     success: function(data){
+      if (data.err_code){
+        var resultHTML = '<p class="result">Alias já utilizado</p>';
+        $('#link').html(resultHTML);
+        $('#link').hide().fadeIn('slow');
+      } else{
         var resultHTML = '<a class="result" href= "http://localhost:3000/' + data.shortUrl + '"> http://localhost:3000/' + data.shortUrl + '</a>';
         $('#link').html(resultHTML);
         $('#link').hide().fadeIn('slow');
+      }
     }
   });
 
